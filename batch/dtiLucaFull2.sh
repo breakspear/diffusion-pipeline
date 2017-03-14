5ab027b739ba1920eb83885b71a80050bc2b4b20
AlistairPerry <alistairperry123@gmail.com>
2017-03-15 02:32:36 +1000

    Fix bug of not being able to find MRtrix directory

--- batch/dtiLucaFull2.sh
+++ batch/dtiLucaFull2.sh
@@ -4,12 +4,12 @@
 # All required resource statements start with "#PBS -l"
 # These will be interpreted by the job submission system (PBS pro)
 
-#PBS -v MRTRIX=/software/mrtrix3/mrtrix3-AP,ncpus=8
 #PBS -l ncpus=8,mem=2G
-#
 # *select* is the number of parallel processes
 # *ncpus* is the number of cores required by each process
 # *mem* is the amount of memory required by each process
+
+#PBS -v MRTRIX=/software/mrtrix3/mrtrix3-AP,ncpus=8
 
 #PBS -l walltime=47:00:00
 # *walltime* is the total time required in hours:minutes:seconds
@@ -31,10 +31,10 @@ module load freesurfer/5.3.0
 module load ANTs/20160509
 module load fsl/5.0.9_eddy
 
-subj=Patient_043
-subjDIRECTORY=/working/lab_michaebr/alistaiP/diffusion-testing/PARK/Raw/Patient_043
+subj=LucaFull2
+subjDIRECTORY=/working/lab_michaebr/alistaiP/diffusion-testing/testPBS/Raw/LucaFull2
 
 cd $subjDIRECTORY
 
-advfulldiffsetup_phil Patient_043 /working/lab_michaebr/alistaiP/diffusion-testing/PARK  #"%%%%" will match to the string  that corresponds to your personalised batch script, or preferred pipeline
+advfulldiffsetup LucaFull2 /working/lab_michaebr/alistaiP/diffusion-testing/testPBS  #"%%%%" will match to the string  that corresponds to your personalised batch script, or preferred pipeline
  
