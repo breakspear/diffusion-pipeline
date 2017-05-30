a42d8a8fa3ce4d64e7f74149e9edda9fcae16e40
AlistairPerry <alistairperry123@gmail.com>
2017-05-30 16:30:05 +1000

    fix script - have to wait for parameters calc

--- miscscripts/Extract_motionparameters_adhoc.R
+++ miscscripts/Extract_motionparameters_adhoc.R
@@ -1,20 +1,21 @@
 #!/usr/bin/env Rscript
 
-args <- commandArgs(TRUE)
+args = commandArgs(TRUE)
 
 #ph<-{} 
 datatablefile="/working/lab_michaebr/alistaiP/Park/analysis/Patients_trackconnectivity.dat"
 ph=read.table(datatablefile,header=T,stringsAsFactors=T,sep="\t",colClasses="character")
 
-#DATADIR="/mnt/lustre/working/lab_michaebr/alistaiP/Park/Diff"
-DATADIR=as.character(args[1])
-#OUTDIR="/working/lab_michaebr/alistaiP/Park/analysis"
-OUTDIR=as.character(args[2])
+DATADIR="/mnt/lustre/working/lab_michaebr/alistaiP/Park/Diff"
+#DATADIR=as.character(args[1])
+OUTDIR="/working/lab_michaebr/alistaiP/Park/analysis"
+#OUTDIR=as.character(args[2])
 
 nvols<-102
 
 PatientIDs<-list.dirs(path = DATADIR, full.names = FALSE, recursive = FALSE)
 PatientIDs<-t(PatientIDs)
+PatientIDs<-as.character(PatientIDs)
 IDs_nopre<-gsub("^.*?_","",PatientIDs)
 
 FDall=NULL
@@ -22,15 +23,21 @@ meanFDall=NULL
 
 for(i in 1:length(PatientIDs))
 {
-  PatDATDir<-paste(DATADIR,PatientIDs[,i],"preproc",sep="/")
-  
+  PatDATDir<-paste(DATADIR,PatientIDs[i],"preproc",sep="/")
+ 
   if (file.exists(paste(PatDATDir,"FD.txt",sep="/"))) {
       meanFDfile=paste(PatDATDir, "meanFD.txt", sep="/")
       meanFDval=read.table(meanFDfile,header=FALSE)
 
       FDfile=paste(PatDATDir, "FD.txt", sep="/")
       FDvals=read.table(FDfile,header=FALSE)
-    
+      if (length(FDvals)<nvols){
+        FDvals[nvols]<-mean(FDvals)
+        }
+      #if (length(FDvals)>nvols){
+        #FDvals<-FDvals[2:length(FDvals)]
+      #}
+
   } else {
     eddyfoldmatch<-list.files(path = PatDATDir, pattern="preproc", include.dirs = TRUE)
     
@@ -38,9 +45,12 @@ for(i in 1:length(PatientIDs))
     FD=read.table(FDfile,header=FALSE)
     
     FDvals<-FD$V2
-    if (length(FDvals)!=nvols){
-	FDvals[nvols]<-mean(FDvals)
-	}
+    if (length(FDvals)<nvols){
+        FDvals[nvols]<-mean(FDvals)
+        }
+    #if (length(FDvals)>nvols){
+    #    FDvals<-FDvals[2:length(FDvals)]
+    #}
 
     meanFDval<-mean(FDvals)
     
