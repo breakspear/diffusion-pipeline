e55e79348a22f473c1eb5ba1d3ba4200df90f730
AlistairPerry <alistairperry123@gmail.com>
2017-12-05 13:11:37 +1000

    realign R scripts w master

---
+++ miscscripts/Extract_motionparameters.R
@@ -0,0 +1,70 @@
+#!/usr/bin/env Rscript
+
+
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+}
+
+args = commandArgs(trailingOnly=TRUE)
+print(args)
+
+#DATADIR="/mnt/lustre/working/lab_michaebr/alistaiP/Park/Diff"
+WORKDIR=as.character(args[1])
+DATADIR=paste(WORKDIR,"Diff",sep="/")
+#OUTDIR="/working/lab_michaebr/alistaiP/Park/analysis"
+OUTDIR=paste(WORKDIR,"QC",sep="/")
+dir.create(OUTDIR)
+
+
+SubjIDs<-list.dirs(path = DATADIR, full.names = FALSE, recursive = FALSE)
+SubjIDs<-t(SubjIDs)
+SubjIDs<-as.character(SubjIDs)
+#IDs_nopre<-gsub("^.*?_","",SubjIDs)
+
+FDall=NULL
+meanFDall=NULL
+
+for(i in 1:length(SubjIDs))
+{
+ 
+    SubjDatDir<-paste(DATADIR,SubjIDs[i],"preproc",sep="/")
+ 
+    eddyfoldmatch<-list.files(path = SubjDatDir, pattern="dwipreproc", include.dirs = TRUE)
+    
+    FDfile=paste(SubjDatDir, eddyfoldmatch, "dwi_post_eddy.eddy_movement_rms", sep="/")
+    
+    print(FDfile)
+    FD=read.table(FDfile,header=FALSE)
+    
+    #determine # of files - can just do from 1st subject
+    if (i==1){
+    nvols<-length(FD[,1])
+}
+
+    FDvals<-FD$V2
+    meanFDval<-mean(FDvals)
+  
+    FDout=c(SubjIDs[i],FDvals)
+    FDall=rbind(FDall,FDout)
+  
+    meanFDout=c(SubjIDs[i],meanFDval)
+    meanFDall=rbind(meanFDall,meanFDout)
+  
+}
+
+meanFDall=data.frame(meanFDall,stringsAsFactors=F)
+names(meanFDall)=c("ID","meanFD")
+
+FDall=data.frame(FDall,stringsAsFactors=F)
+names(FDall)=c("ID",paste("VOL",seq(1,nvols),sep=""))
+
+#check if motion parameters are to be integrated into existing data table
+
+tmp<-meanFDall
+meanFDfname.out=paste(OUTDIR,"Subjects_meanFD.dat",sep="/")
+
+#write output
+write.table(tmp,file=meanFDfname.out,row.names=F,col.names=T,sep="\t",quote=F)
+
+FDallfname.out=paste(OUTDIR,"Subjects_FDall.dat",sep="/")
+write.table(FDall,file=FDallfname.out,row.names=F,col.names=T,sep="\t",quote=F)
---
+++ philscripts/Extract_seedconnectivity.R
@@ -0,0 +1,66 @@
+#!/usr/bin/env Rscript
+
+
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+}
+print(args)
+
+#DATADIR="/mnt/lustre/working/lab_michaebr/alistaiP/Park/Diff"
+DATADIR=as.character(args[1])
+
+if (length(args)==2) {
+preextdata<-as.character(args[2])
+}
+
+SubjIDs<-list.dirs(path = DATADIR, full.names = FALSE, recursive = FALSE)
+SubjIDs<-t(SubjIDs)
+SubjIDs<-as.character(SubjIDs)
+
+convars=cbind("meanFA","meanMD","AFD")
+
+convarstrings = NULL
+for(i in 1:length(convars))
+{
+  varmatch<-list.files(path = paste(DATADIR,egsubject,sep="/"), pattern=paste(convars[i],".txt",sep=""))
+  convarstrings=c(convarstrings,varmatch)
+}
+
+varnames<-gsub(pattern = "\\.txt$", "", convarstrings)
+
+xall=NULL
+
+for(i in 1:length(SubjIDs))
+{
+
+SubjDir<-paste(DATADIR,SubjIDs[i],sep="/")
+  
+xx=NULL
+for(j in 1:length(convarstrings))
+{
+confile=paste(SubjDir, convarstrings[j], sep="/")
+condata=scan(confile)
+xx=c(xx,condata)
+}
+
+out=c(SubjIDs[i],xx)
+xall=rbind(xall,out)
+
+}
+
+xall=data.frame(xall,stringsAsFactors=F)
+names(xall)=c("ID",varnames)
+
+if( exists("preextdata") {
+
+fname.out=paste(DATADIR,"Subjs_seedconnectivity_wprexdata.dat",sep="/")
+tmp=merge(preextdata,xall,by.x="ID",by.y="ID",sort=F)
+write.table(tmp,file=fname.out,row.names=F,col.names=T,sep="\t",quote=F)
+
+} else {
+
+fname.out=paste(DATADIR,"Subjs_seedconnectivity.dat",sep="/")
+write.table(xall,file=fname.out,row.names=F,col.names=T,sep="\t",quote=F)
+
+}
