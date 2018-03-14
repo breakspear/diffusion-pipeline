d4d9a987d051d61f81e7f61fea430e77ccf22773
nikitas-k <31430935+nikitas-k@users.noreply.github.com>
2018-03-14 15:13:03 +1000

    wrong save format

--- nikscripts/cntme_PE002.py
+++ nikscripts/cntme_PE002.py
@@ -28,9 +28,7 @@ def main():
         else:
           array[i,j] = 0
 
-    file = open(PE002_cntme.txt,'w')
-    file.write(array)
-    file.close()
+    np.savetxt('PE002_cntme.txt', array)
 
 if __name__ == "__main__":
     print("Processing subject AFD connectome from files. This may take a long time...")
