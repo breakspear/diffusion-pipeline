134c7c7b625dad94cdfe2f3530def422a5c93f4e
aperry <alistairperry123@gmail.com>
2018-03-25 13:19:32 +0200

    KFO updates

---
+++ fixelanalysis/fixelbatch/fixel_AFD2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH -n 1 # 1 per CPU
+#SBATCH -c 5 # number of cpus per task
+#SBATCH --nice=5
+#SBATCH -p blade-a
+
+source /daten/bin/setup_fsl-5.0.9-cscor.sh
+source /beegfs/v1/bin2/setup_freesurfer_530.sh
+
+MRTRIX=/beegfs/scratch/tnc_scratch/aperry/MRtrix/02032017
+export PATH=${MRTRIX}/release/bin:${MRTRIX}/scripts:$PATH
+
+export OMP_NUM_THREADS=8
+
+####
+#Find all scripting files
+#currdir=$(pwd)
+pipedir=/beegfs/scratch/tnc_scratch/kfo_pd_connectome/PD_connectome/diffusion-pipeline/fixelanalysis
+PATH=$PATH$( find $pipedir/ -not -path '*/\.*'  -type d -printf ":%p" )
+#export PATH:$PATH
+
+AFD2  /beegfs/scratch/tnc_scratch/kfo_pd_connectome/PD_connectome 1.5 upsample #"%%%%" will match to the string  that corresponds to your personalised batch script, or preferred pipeline
